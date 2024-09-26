9e33dbcb8f05afd529ef1454bef7f41feaa6df99
ilker <ilker@okutman.com>
2024-09-26 16:15:40 +0300

    replace pin screen with lock screen

--- lib/app/presentation/screens/pin/pin_screen.dart
+++ lib/app/presentation/screens/pin/pin_screen.dart
@@ -1,32 +1,11 @@
-import 'package:central_heating_control/app/data/services/nav.dart';
+import 'package:central_heating_control/app/presentation/screens/lock_screen/lock_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-class PinScreen extends StatefulWidget {
+class PinScreen extends StatelessWidget {
   const PinScreen({super.key});
 
   @override
-  State<PinScreen> createState() => _PinScreenState();
-}
-
-class _PinScreenState extends State<PinScreen> {
-  late String username;
-
-  @override
-  void initState() {
-    super.initState();
-    username = Get.parameters['username'] ?? '';
-    if (username.isEmpty) {
-      NavController.lock();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Pin Screen'),
-      ),
-    );
+    return const LockScreen();
   }
 }
