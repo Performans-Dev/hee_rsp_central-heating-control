afd90d0c4530f61e6e6804ea20676485fe62d625
ilker <ilker@okutman.com>
2024-09-27 15:39:07 +0300

    adding new hardware step2

--- lib/app/presentation/screens/settings/preferences/advanced/hardware_config/settings_preferences_advanced_hardware_addnew_screen.dart
+++ lib/app/presentation/screens/settings/preferences/advanced/hardware_config/settings_preferences_advanced_hardware_addnew_screen.dart
@@ -1,9 +1,35 @@
+import 'package:central_heating_control/app/data/models/hardware_extension.dart';
+import 'package:central_heating_control/app/data/providers/static_provider.dart';
 import 'package:central_heating_control/app/presentation/components/app_scaffold.dart';
+import 'package:central_heating_control/app/presentation/components/pi_scroll.dart';
+import 'package:central_heating_control/app/presentation/widgets/text_input.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPreferencesAdvancedHardwareConfigAddNewScreen
-    extends StatelessWidget {
+    extends StatefulWidget {
   const SettingsPreferencesAdvancedHardwareConfigAddNewScreen({super.key});
+
+  @override
+  State<SettingsPreferencesAdvancedHardwareConfigAddNewScreen> createState() =>
+      _SettingsPreferencesAdvancedHardwareConfigAddNewScreenState();
+}
+
+class _SettingsPreferencesAdvancedHardwareConfigAddNewScreenState
+    extends State<SettingsPreferencesAdvancedHardwareConfigAddNewScreen> {
+  HardwareExtension? selectedHardwareExtension;
+  late TextEditingController serialNumberController;
+
+  @override
+  void initState() {
+    super.initState();
+    serialNumberController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    serialNumberController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +39,69 @@ class SettingsPreferencesAdvancedHardwareConfigAddNewScreen
       body: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          Text('pick from dropdown'),
-          Text('display selected hw details'),
-          Text('ask for serial number'),
-          Text('confirm btn'),
+          DropdownMenu<HardwareExtension>(
+            enableFilter: false,
+            enableSearch: false,
+            leadingIcon: Icon(Icons.hardware),
+            dropdownMenuEntries: StaticProvider.availableHardwareExtensions
+                .map((e) => DropdownMenuEntry<HardwareExtension>(
+                    label: e.modelName, value: e))
+                .toList(),
+            label: Text('Available Hardware Profiles'),
+            onSelected: (value) {
+              setState(() {
+                selectedHardwareExtension = value;
+              });
+            },
+          ),
+          Divider(),
+          Expanded(
+            child: selectedHardwareExtension == null
+                ? Center(
+                    child: Text('Select a hardware profile from list'),
+                  )
+                : PiScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          title: Text('Manufacturer'),
+                          trailing: Text(
+                              '${selectedHardwareExtension?.manufacturer}'),
+                        ),
+                        ListTile(
+                          title: Text('Model'),
+                          trailing:
+                              Text('${selectedHardwareExtension?.modelName}'),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('ADC: ${selectedHardwareExtension?.adcCount}'),
+                            Text('DAC: ${selectedHardwareExtension?.dacCount}'),
+                            Text('DI: ${selectedHardwareExtension?.diCount}'),
+                            Text('DO: ${selectedHardwareExtension?.doCount}'),
+                          ],
+                        ),
+                        ListTile(
+                          title: Text('Connection'),
+                          subtitle: Text(selectedHardwareExtension!
+                              .connectionType
+                              .join(', ')),
+                        ),
+                        Divider(),
+                        TextInputWidget(
+                          controller: serialNumberController,
+                          labelText: 'Serial number',
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Confirm Adding Hardware'),
+                        ),
+                      ],
+                    ),
+                  ),
+          ),
         ],
       ),
     );
