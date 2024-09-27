f2ccbd1d4bb7d9ec9ffc5e58bb3bd315e0dd7e73
ilker <ilker@okutman.com>
2024-09-27 16:50:24 +0300

    added try-catch to logger

--- lib/app/data/providers/log.dart
+++ lib/app/data/providers/log.dart
@@ -8,79 +8,89 @@ import 'package:path/path.dart' as path;
 
 class LogService {
   static Future<String> getLogFilePath({DateTime? date}) async {
-    final now = date ?? DateTime.now();
-    final logDir = path.join(
-        Box.documentsDirectoryPath,
-        Keys.logPath,
-        now.year.toString(),
-        now.month.toString().padLeft(2, '0'),
-        now.day.toString().padLeft(2, '0'));
+    try {
+      final now = date ?? DateTime.now();
+      final logDir = path.join(
+          Box.documentsDirectoryPath,
+          Keys.logPath,
+          now.year.toString(),
+          now.month.toString().padLeft(2, '0'),
+          now.day.toString().padLeft(2, '0'));
 
-    // Ensure the directory exists
-    await Directory(logDir).create(recursive: true);
+      // Ensure the directory exists
+      await Directory(logDir).create(recursive: true);
 
-    return path.join(logDir, 'logs.json');
+      return path.join(logDir, 'logs.json');
+    } on Exception catch (_) {
+      return '/home/pi/Heethings/CC/logs';
+    }
   }
 
   static Future<void> log(LogDefinition logEntry) async {
-    final logFilePath = await getLogFilePath();
-    final logFile = File(logFilePath);
+    try {
+      final logFilePath = await getLogFilePath();
+      final logFile = File(logFilePath);
 
-    List<LogDefinition> logs = [];
+      List<LogDefinition> logs = [];
 
-    if (await logFile.exists()) {
-      final content = await logFile.readAsString();
-      if (content.isNotEmpty) {
-        final decoded = jsonDecode(content);
-        logs = List<Map<String, dynamic>>.from(decoded)
-            .map((map) => LogDefinition.fromMap(map))
-            .toList();
+      if (await logFile.exists()) {
+        final content = await logFile.readAsString();
+        if (content.isNotEmpty) {
+          final decoded = jsonDecode(content);
+          logs = List<Map<String, dynamic>>.from(decoded)
+              .map((map) => LogDefinition.fromMap(map))
+              .toList();
+        }
       }
-    }
 
-    logs.add(logEntry);
+      logs.add(logEntry);
 
-    final encodedLogs = jsonEncode(logs.map((log) => log.toMap()).toList());
-    await logFile.writeAsString(encodedLogs, mode: FileMode.write);
+      final encodedLogs = jsonEncode(logs.map((log) => log.toMap()).toList());
+      await logFile.writeAsString(encodedLogs, mode: FileMode.write);
+    } on Exception catch (_) {}
   }
 
   static Future<void> addLog(LogDefinition logEntry) async {
-    final logFilePath = await getLogFilePath();
-    final logFile = File(logFilePath);
+    try {
+      final logFilePath = await getLogFilePath();
+      final logFile = File(logFilePath);
 
-    List<LogDefinition> logs = [];
+      List<LogDefinition> logs = [];
 
-    if (await logFile.exists()) {
-      final content = await logFile.readAsString();
-      if (content.isNotEmpty) {
-        final decoded = jsonDecode(content);
-        logs = List<Map<String, dynamic>>.from(decoded)
-            .map((map) => LogDefinition.fromMap(map))
-            .toList();
+      if (await logFile.exists()) {
+        final content = await logFile.readAsString();
+        if (content.isNotEmpty) {
+          final decoded = jsonDecode(content);
+          logs = List<Map<String, dynamic>>.from(decoded)
+              .map((map) => LogDefinition.fromMap(map))
+              .toList();
+        }
       }
-    }
 
-    // Append the new log entry to the existing list
-    logs.add(logEntry);
+      // Append the new log entry to the existing list
+      logs.add(logEntry);
 
-    // Write the updated list back to the file
-    final encodedLogs = jsonEncode(logs.map((log) => log.toMap()).toList());
-    await logFile.writeAsString(encodedLogs, mode: FileMode.write);
+      // Write the updated list back to the file
+      final encodedLogs = jsonEncode(logs.map((log) => log.toMap()).toList());
+      await logFile.writeAsString(encodedLogs, mode: FileMode.write);
+    } on Exception catch (_) {}
   }
 
   static Future<List<LogDefinition>> readLogs({DateTime? date}) async {
-    final logFilePath = await getLogFilePath(date: date);
-    final logFile = File(logFilePath);
+    try {
+      final logFilePath = await getLogFilePath(date: date);
+      final logFile = File(logFilePath);
 
-    if (await logFile.exists()) {
-      final content = await logFile.readAsString();
-      if (content.isNotEmpty) {
-        final decoded = jsonDecode(content);
-        return List<Map<String, dynamic>>.from(decoded)
-            .map((map) => LogDefinition.fromMap(map))
-            .toList();
+      if (await logFile.exists()) {
+        final content = await logFile.readAsString();
+        if (content.isNotEmpty) {
+          final decoded = jsonDecode(content);
+          return List<Map<String, dynamic>>.from(decoded)
+              .map((map) => LogDefinition.fromMap(map))
+              .toList();
+        }
       }
-    }
+    } on Exception catch (_) {}
 
     return [];
   }
