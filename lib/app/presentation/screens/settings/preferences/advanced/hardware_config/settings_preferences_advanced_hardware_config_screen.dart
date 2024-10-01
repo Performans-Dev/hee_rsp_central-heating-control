8f798178caf9e6c9666c3899f095809da3b9a170
ilker <ilker@okutman.com>
2024-10-01 22:57:54 +0300

    edit hw

--- lib/app/presentation/screens/settings/preferences/advanced/hardware_config/settings_preferences_advanced_hardware_config_screen.dart
+++ lib/app/presentation/screens/settings/preferences/advanced/hardware_config/settings_preferences_advanced_hardware_config_screen.dart
@@ -4,6 +4,7 @@ import 'package:central_heating_control/app/data/services/data.dart';
 import 'package:central_heating_control/app/presentation/components/app_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:on_screen_keyboard_tr/on_screen_keyboard_tr.dart';
 
 class SettingsPreferencesAdvancedHardwareConfigScreen extends StatelessWidget {
   const SettingsPreferencesAdvancedHardwareConfigScreen({super.key});
@@ -39,26 +40,57 @@ class SettingsPreferencesAdvancedHardwareConfigScreen extends StatelessWidget {
                     )
                   : ListView.builder(
                       itemBuilder: (context, index) => ListTile(
-                        title: Text(dc.hardwareExtensionList[index].modelName),
-                        leading: const Icon(Icons.hardware),
-                        subtitle: Text(
-                            'ID: ${dc.hardwareExtensionList[index].deviceId} Serial: ${dc.hardwareExtensionList[index].serialNumber}'),
-                        trailing: IconButton(
-                            onPressed: () {
-                              DialogUtils.confirmDialog(
-                                  context: context,
-                                  title: 'Delete Hardware',
-                                  description:
-                                      'Are you sure you want to delete this hardware extension?',
-                                  positiveText: 'Delete',
-                                  negativeText: 'Cancel',
-                                  positiveCallback: () async {
-                                    await dc.deleteHardware(
-                                        dc.hardwareExtensionList[index]);
-                                  });
-                            },
-                            icon: const Icon(Icons.delete)),
-                      ),
+                          title:
+                              Text(dc.hardwareExtensionList[index].modelName),
+                          leading: const Icon(Icons.hardware),
+                          subtitle: Text(
+                              'ID: ${dc.hardwareExtensionList[index].deviceId} Serial: ${dc.hardwareExtensionList[index].serialNumber}'),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextButton.icon(
+                                onPressed: () async {
+                                  final result = await OnScreenKeyboard.show(
+                                    context: context,
+                                    initialValue: dc
+                                        .hardwareExtensionList[index]
+                                        .serialNumber,
+                                  );
+                                  // update result
+                                },
+                                label: Text('SerialNumber'),
+                                icon: const Icon(Icons.edit),
+                              ),
+                              TextButton.icon(
+                                onPressed: () async {
+                                  final result = await OnScreenKeyboard.show(
+                                    context: context,
+                                    initialValue: dc
+                                        .hardwareExtensionList[index].deviceId,
+                                  );
+                                  // update result
+                                },
+                                label: Text('ID'),
+                                icon: const Icon(Icons.edit),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  DialogUtils.confirmDialog(
+                                      context: context,
+                                      title: 'Delete Hardware',
+                                      description:
+                                          'Are you sure you want to delete this hardware extension?',
+                                      positiveText: 'Delete',
+                                      negativeText: 'Cancel',
+                                      positiveCallback: () async {
+                                        await dc.deleteHardware(
+                                            dc.hardwareExtensionList[index]);
+                                      });
+                                },
+                                icon: const Icon(Icons.delete),
+                              ),
+                            ],
+                          )),
                       itemCount: dc.hardwareExtensionList.length,
                     ),
             ),
