73aeac6152f3aa3734dc0293ed4305057b94c7ee
ilker <ilker@okutman.com>
2024-10-02 13:01:14 +0300

    hw list update

--- lib/app/presentation/screens/settings/preferences/advanced/hardware_config/settings_preferences_advanced_hardware_config_screen.dart
+++ lib/app/presentation/screens/settings/preferences/advanced/hardware_config/settings_preferences_advanced_hardware_config_screen.dart
@@ -4,7 +4,6 @@ import 'package:central_heating_control/app/data/services/data.dart';
 import 'package:central_heating_control/app/presentation/components/app_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:on_screen_keyboard_tr/on_screen_keyboard_tr.dart';
 
 class SettingsPreferencesAdvancedHardwareConfigScreen extends StatelessWidget {
   const SettingsPreferencesAdvancedHardwareConfigScreen({super.key});
@@ -40,14 +39,53 @@ class SettingsPreferencesAdvancedHardwareConfigScreen extends StatelessWidget {
                     )
                   : ListView.builder(
                       itemBuilder: (context, index) => ListTile(
-                          title:
-                              Text(dc.hardwareExtensionList[index].modelName),
-                          leading: const Icon(Icons.hardware),
-                          subtitle: Text(
-                              'ID: ${dc.hardwareExtensionList[index].id}'
-                              'deviceId: ${dc.hardwareExtensionList[index].deviceId} '
-                              'Serial: ${dc.hardwareExtensionList[index].serialNumber}'),
-                          trailing: Row(
+                        title: Text(
+                            '${dc.hardwareExtensionList[index].modelName} (${dc.hardwareExtensionList[index].connectionType.join(',')})'),
+                        leading: const Icon(Icons.hardware),
+                        subtitle: Text(
+                            // 'ID: ${dc.hardwareExtensionList[index].id}, '
+                            'deviceId: ${dc.hardwareExtensionList[index].deviceId}, '
+                            'Serial: ${dc.hardwareExtensionList[index].serialNumber}'),
+                        trailing: PopupMenuButton(
+                          child: const Icon(Icons.more_vert),
+                          itemBuilder: (context) => [
+                            PopupMenuItem(
+                              child: const ListTile(
+                                leading: Icon(Icons.edit),
+                                title: Text('Change Serial Number'),
+                              ),
+                              onTap: () {},
+                            ),
+                            PopupMenuItem(
+                              child: const ListTile(
+                                leading: Icon(Icons.edit),
+                                title: Text('Change Device ID'),
+                              ),
+                              onTap: () {},
+                            ),
+                            PopupMenuItem(
+                              child: const ListTile(
+                                leading: Icon(Icons.delete),
+                                title: Text('Delete'),
+                              ),
+                              onTap: () {
+                                DialogUtils.confirmDialog(
+                                    context: context,
+                                    title: 'Delete Hardware',
+                                    description:
+                                        'Are you sure you want to delete this hardware extension?',
+                                    positiveText: 'Delete',
+                                    negativeText: 'Cancel',
+                                    positiveCallback: () async {
+                                      await dc.deleteHardware(
+                                          dc.hardwareExtensionList[index]);
+                                    });
+                              },
+                            ),
+                          ],
+                          onSelected: (value) {},
+                        ),
+                        /* Row(
                             mainAxisSize: MainAxisSize.min,
                             children: [
                               TextButton.icon(
@@ -92,7 +130,8 @@ class SettingsPreferencesAdvancedHardwareConfigScreen extends StatelessWidget {
                                 icon: const Icon(Icons.delete),
                               ),
                             ],
-                          )),
+                          ), */
+                      ),
                       itemCount: dc.hardwareExtensionList.length,
                     ),
             ),
