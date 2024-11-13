44bb0b073c923587866e3a5ce131efaaa6176fde
ilker <ilker@okutman.com>
2024-11-13 12:57:14 +0300

    i3

--- lib/app/presentation/screens/misc/pi_info/pi_info_screen.dart
+++ lib/app/presentation/screens/misc/pi_info/pi_info_screen.dart
@@ -9,106 +9,82 @@ class PiInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          Container(
-            constraints: BoxConstraints.expand(),
-            child: GetBuilder<AppController>(builder: (app) {
-              return Material(
-                child: Center(
-                  child: Container(
-                    color: Colors.white.withOpacity(0.9),
-                    margin: EdgeInsets.symmetric(horizontal: 60, vertical: 50),
-                    padding: EdgeInsets.all(20),
+      backgroundColor: Colors.white.withOpacity(0.9),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actionsIconTheme: const IconThemeData(color: Colors.black87),
+        title: const Text('Pi Info'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () => Get.back(),
+          ),
+        ],
+      ),
+      body: GetBuilder<AppController>(
+        builder: (app) {
+          return Center(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Container(
+                    color: Colors.white,
+                    height: double.infinity,
+                    alignment: Alignment.topCenter,
+                    padding: const EdgeInsets.all(4),
+                    margin: const EdgeInsets.all(4),
                     child: Column(
-                      mainAxisSize: MainAxisSize.max,
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'test',
-                              style: TextStyle(color: Colors.black87),
-                            ),
-                            IconButton(
-                              icon: Icon(
-                                Icons.close,
-                                color: Colors.black87,
-                              ),
-                              onPressed: () => Get.back(),
-                            ),
-                          ],
+                        const Text(
+                          'Serial Number',
+                          style: TextStyle(color: Colors.black),
                         ),
-                        Divider(),
-                        Expanded(
-                          child: Row(
-                            children: [
-                              Container(
-                                color: Colors.white,
-                                height: double.infinity,
-                                alignment: Alignment.topCenter,
-                                padding: EdgeInsets.all(4),
-                                margin: EdgeInsets.all(4),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      'Serial Number',
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                    QrImageView(
-                                      data:
-                                          app.deviceInfo?.serialNumber ?? 'N/A',
-                                      version: QrVersions.auto,
-                                      size: 200.0,
-                                      backgroundColor: Colors.white,
-                                      eyeStyle: QrEyeStyle(
-                                        color: Colors.black87,
-                                        eyeShape: QrEyeShape.square,
-                                      ),
-                                      dataModuleStyle: QrDataModuleStyle(
-                                        color: Colors.black87,
-                                        dataModuleShape:
-                                            QrDataModuleShape.square,
-                                      ),
-                                    ),
-                                    Text(
-                                      app.deviceInfo?.serialNumber ?? 'N/A',
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                color: Colors.white,
-                                height: double.infinity,
-                                alignment: Alignment.topCenter,
-                                padding: EdgeInsets.all(4),
-                                margin: EdgeInsets.all(4),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      'Connectivity',
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                    Text('eth0:'),
-                                    Text('wifi:'),
-                                  ],
-                                ),
-                              ),
-                            ],
+                        QrImageView(
+                          data: app.deviceInfo?.serialNumber ?? 'N/A',
+                          version: QrVersions.auto,
+                          size: 200.0,
+                          backgroundColor: Colors.white,
+                          eyeStyle: const QrEyeStyle(
+                            color: Colors.black87,
+                            eyeShape: QrEyeShape.square,
                           ),
+                          dataModuleStyle: const QrDataModuleStyle(
+                            color: Colors.black87,
+                            dataModuleShape: QrDataModuleShape.square,
+                          ),
+                        ),
+                        Text(
+                          app.deviceInfo?.serialNumber ?? 'N/A',
+                          style: const TextStyle(color: Colors.black),
                         ),
                       ],
                     ),
                   ),
-                ),
-              );
-            }),
-          )
-        ],
+                  Container(
+                    color: Colors.white,
+                    height: double.infinity,
+                    alignment: Alignment.topCenter,
+                    padding: const EdgeInsets.all(4),
+                    margin: const EdgeInsets.all(4),
+                    child: const Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Connectivity',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Text('eth0:'),
+                        Text('wifi:'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
