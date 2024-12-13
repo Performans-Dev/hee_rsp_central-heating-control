2dcc15269f5c574d068ce9fe981bcc6d5cf35262
ilker <ilker@okutman.com>
2024-12-13 13:36:59 +0300

    q4

--- lib/app/presentation/components/dropdowns/channel.dart
+++ lib/app/presentation/components/dropdowns/channel.dart
@@ -10,20 +10,24 @@ class ChannelDropdownWidget extends StatelessWidget {
     super.key,
     this.onChanged,
     this.group,
+    this.value,
   });
   final Function(int?)? onChanged;
   final GpioGroup? group;
+  final int? value;
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<ChannelController>(builder: (cc) {
       return GetBuilder<DataController>(builder: (dc) {
         final List<ChannelDefinition> data = group == GpioGroup.outPin
-            ? cc.outputChannels
+            ? cc.outputChannels.where((e) => e.userSelectable).toList()
             : group == GpioGroup.inPin
-                ? cc.inputChannels
+                ? cc.inputChannels.where((e) => e.userSelectable).toList()
                 : [];
         final occupiedIds = [3, 5];
+        var selectedValue = value;
+        selectedValue ??= data.first.id;
 
         return ClipRRect(
           borderRadius: UiDimens.formRadius,
@@ -32,13 +36,16 @@ class ChannelDropdownWidget extends StatelessWidget {
             isExpanded: true,
             padding: const EdgeInsets.symmetric(horizontal: 8),
             items: data.map((e) {
+              bool own = e.id == value;
               return DropdownMenuItem<int>(
                 value: e.id,
-                enabled: !occupiedIds.contains(e.id),
+                enabled: !occupiedIds.contains(e.id) && !own,
                 child: Text(e.name),
               );
             }).toList(),
             onChanged: onChanged,
+            value: value,
+            borderRadius: UiDimens.formRadius,
           ),
         );
         /*  final l1List = dc.heaterList
